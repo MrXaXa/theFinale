e02efac5b70ba31b44a45777e6271ea5a38757e6
MrXaXa <temperfox123@gmail.com>
2020-05-20 13:51:21 -0600

    Made the basic framework for the panel, still need to find a tutorial on how to get java objects moving.

--- src/gameView/gamePanel.java
+++ src/gameView/gamePanel.java
@@ -1,6 +1,41 @@
 package gameView;
 
+import finalController.Controller;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Dimension;
+
 public class gamePanel extends JPanel
 {
-
+	private Controller controller;
+	private SpringLayout appLayout;
+	
+	public gamePanel(Controller controller)
+	{
+		
+	}
+	
+	private void setupDropDown()
+	{
+		
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	
+	
 }
